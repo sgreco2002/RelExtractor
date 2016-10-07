f4c449bf5c2ac003ec6ed61476120d687466be57
sgreco2002 <sgreco2002@libero.it>
2016-10-07 19:13:29 +0200

    Update relgetter.text.sh

--- relgetter.text.sh
+++ relgetter.text.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-readarray PKGLIST < /home/custapache/RelExtractor/package.list
+USR=`whoami`
+readarray PKGLIST < /home/$USR/RelExtractor/package.list
 for ITERATOR in ${PKGLIST[@]}; do
         IFS=# read ART PKG FILE GREP <<< "$ITERATOR"
         RELVAL=`less $PKG/$FILE | grep -i $GREP`
