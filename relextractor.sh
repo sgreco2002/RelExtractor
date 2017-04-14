14640381b75e699621503528a15d30e17f2d8c6c
sgreco2002 <sgreco2002@libero.it>
2017-04-14 10:22:47 +0200

    Update relextractor.sh

--- relextractor.sh
+++ relextractor.sh
@@ -2,8 +2,8 @@
 USR=`whoami`
 readarray PKGLIST < /home/$USR/RelExtractor/package.list
 for ITERATOR in ${PKGLIST[@]}; do
-        IFS=#  read ART SHL DB <<< "$ITERATOR"
-        RELVAL=`$SHL $DB`
+        IFS=# read ART PKG FILE GREP <<< "$ITERATOR"
+        RELVAL=`unzip -qc $PKG $FILE | grep -i $GREP`
         echo "$ART#$RELVAL"
 done
 exit 0
